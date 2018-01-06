d513e18e6202b1679a59e0ebba9a6a31770954f3
BajaCali <michaelnemec47@gmail.com>
2018-01-06 12:50:27 +0100

    Init of project

---
+++ src/credentials.h
@@ -0,0 +1,4 @@
+#pragma once
+
+static const char* ssid = "name_of_wifi";
+static const char* password = "wifi_key";
---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+/*
+ *MQTT node for smart home system
+ *
+ *Created: 6. 1. 2018
+ *Author: BajaCali
+ *
+ */
+
+#include <Arduino.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// wifi name and pass in "credentials.h"
+#include "credentials.h"
+
+void setup() {
+    // put your setup code here, to run once:
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
